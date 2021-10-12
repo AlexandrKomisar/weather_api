e33d101f2655127abb56ae47466eca6ba51e36e8
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-12 17:25:52 +0300

    arr

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?id=6548066&appid=b23aa5b1fc2f5ed96f3147131acfce5f')
+fetch('http://api.openweathermap.org/data/2.5/weather?id=6548066&appid=b23aa5b1fc2f5ed96f3147131acfce5f&lang=ru')
 
   .then(function (resp) {
     return resp.json()
@@ -7,6 +7,8 @@ fetch('http://api.openweathermap.org/data/2.5/weather?id=6548066&appid=b23aa5b1f
     console.log(data);
     document.querySelector('.city-name').innerHTML = data.name;
     document.querySelector('.weather-temp').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+    document.querySelector('.weather-cloud').innerHTML = data.weather[0]['description'];
+    document.querySelector('.weather-icon').innerHTML = data.weather[0]['icon'];
   })
   .catch(function () {
     // eroros
