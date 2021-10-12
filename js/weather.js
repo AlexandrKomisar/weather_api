d3f1864cf499fe9299d29d73bb4b4ec8e0891a3f
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-12 19:47:45 +0300

    nik

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,7 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?id=6548066&appid=b23aa5b1fc2f5ed96f3147131acfce5f&lang=ru')
-
+// kiev
+// fetch('http://api.openweathermap.org/data/2.5/weather?id=6548066&appid=b23aa5b1fc2f5ed96f3147131acfce5f&lang=ru')
+// mykolaev 700569
+fetch('http://api.openweathermap.org/data/2.5/weather?id=700569&appid=b23aa5b1fc2f5ed96f3147131acfce5f&lang=ru')
   .then(function (resp) {
     return resp.json()
   })
@@ -9,9 +11,13 @@ fetch('http://api.openweathermap.org/data/2.5/weather?id=6548066&appid=b23aa5b1f
     document.querySelector('.weather-temp').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
     document.querySelector('.weather-cloud').innerHTML = data.weather[0]['description'];
     document.querySelector('.weather-icon').innerHTML = data.weather[0]['icon'];
+
+    // document.querySelector('02n').innerHTML = data.weather[0]['icon'];
   })
+  
   .catch(function () {
     // eroros
   });
-
-
+ 
+//  $('li:contains("01d")');
+    // x.html('<img src="./image/02d.png"/>')
